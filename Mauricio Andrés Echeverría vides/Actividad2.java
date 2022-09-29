1216eaec597095cb886fe69f66c94c918255e620
MauricioEcheverriaV <mechevev@emeal.nttdata.com>
2022-09-28 19:47:14 -0500

    Actividad2java

---
+++ Mauricio Andrés Echeverría vides/Actividad2.java
@@ -0,0 +1,96 @@
+public class  Operacion {
+
+  int suma ;
+  int resta ;
+  int multiplicacion ;
+  int division ;
+  boolean noDividirEntreCero ;
+
+
+
+  public Operacion ( ) {
+		
+
+  }
+
+
+
+  public int sumar (int suma , int suma1 ) {
+		print(“Sumar” + suma+suma1 );
+		return suma+suma1;
+
+  }
+
+  public int restar(int resta1, int resta2){
+		print(“restar” - resta1-resta2 );
+		return resta1-resta2;
+
+  }
+
+  public int multiplicar (int multiplicacion1, int multiplicacion2 ) {
+		print(“multiplicar” * multiplicacion1*multiplicacion2 );
+		return multiplicacion1*multiplicacion2;
+
+  }
+
+  public int dividir (int numero1, int numero2   ) {
+		if (numero1 >= 1 & numero2 >= 1 )-> int dividir se ejecuta
+		print(“dividir 6/3 ”);
+		return 2;
+
+
+  }
+
+
+
+  -------------------------------------------------------------------------------------------------------------------
+
+public class Reloj{
+
+	int
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
