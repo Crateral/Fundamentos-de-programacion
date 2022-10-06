fc9ab6fc812539880e6f2ce6e650f3f42d808e0e
arestrepst <114423544+arestrepst@users.noreply.github.com>
2022-10-06 17:31:12 -0500

    Semana2 Actividad 5 punto Extra
    Mejoras al código y Punto adicional

---
+++ Alejandro Restrepo Angel/S2Actividad5_2/Informe.java
@@ -0,0 +1,45 @@
+package S2Actividad5_2;
+
+public class Informe {
+
+    public final String[] tareas={"administrativo", "empresarial", "personal"};
+
+    private int codigo;
+    private String tarea;
+
+    public Informe() {
+        this.codigo= 0;
+        this.tarea= "";
+    }
+
+    public Informe(int codigo,int indiceTarea){
+        this.codigo= codigo;
+        this.tarea= this.tareas[indiceTarea];
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTarea() {
+        return tarea;
+    }
+
+    public void setTarea(String tarea) {
+        this.tarea = tarea;
+    }
+
+    public String[] getTareas(){
+        return tareas;
+    }
+
+    @Override
+    public String toString(){
+        return "El informe con codigo "+codigo+" tiene la tarea de "+tarea;
+    }
+
+}
